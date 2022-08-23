f10c276c683ea1e42027ed463382d26e98b3d92f
BragaJoao <joaogbraga@hotmail.com.br>
2022-08-23 18:36:25 -0300

    mongoDB Atlas

--- src/database/database.js
+++ src/database/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectToDatabase = () => {
   mongoose
-    .connect('mongodb://localhost:27017/lists-db', {
+    .connect(process.env.DATABASE_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
