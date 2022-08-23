bba76696a4f664f32d8beec5dbf287ee16d2c47d
BragaJoao <joaogbraga@hotmail.com.br>
2022-08-23 20:10:08 -0300

    fix

--- src/controllers/list.controller.js
+++ src/controllers/list.controller.js
@@ -1,10 +1,9 @@
 const listsService = require('../services/list.services');
 const mongoose = require('mongoose');
 
-
 const findAllListsController = async (req, res) => {
   const lists = await listsService.findAllListsService();
-  res.status(200).send(lists);
+  res.status(200).send({ phrases: lists });
 };
 
 const findByIdListController = async (req, res) => {
@@ -13,20 +12,20 @@ const findByIdListController = async (req, res) => {
   if (!chosenList) {
     return res.status(404).send({ message: 'Tarefa não encontrada!' });
   }
-  res.status(200).send(chosenList);
+  res.status(200).send({ phrase: chosenList });
 };
 
 const createListController = async (req, res) => {
   const list = req.body;
   const newList = await listsService.createListService(list);
-  res.status(201).send(newList);
+  res.status(201).send({ phrase: newList });
 };
 
 const updateListController = async (req, res) => {
   const id = req.params.id;
   const listEdit = req.body;
   const updatedList = await listsService.updateListService(id, listEdit);
-  res.status(200).send(updatedList);
+  res.status(200).send({ phrase: updatedList });
 };
 
 const deleteListController = async (req, res) => {
